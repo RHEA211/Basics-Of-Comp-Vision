6c500ca6ff8d438f692f06100ffeec65b59561ac
Rhea <64091188+RHEA211@users.noreply.github.com>
2021-06-25 08:32:26 +0530

    Add files via upload

---
+++ 5.GaussianBlur.py
@@ -0,0 +1,8 @@
+#How to apply Gaussian Blur- helpful for smoothening
+
+import cv2
+img=cv2.imread("1.jpg")
+
+ 
+gaussianBlurImg=cv2.GaussianBlur(img,(21,21),0)
+cv2.imwrite("gaussianImage.jpg",gaussianBlurImg)
